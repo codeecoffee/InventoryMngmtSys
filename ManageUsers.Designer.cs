984eebc8efee7eb8f960f247e08782c9e2dc2c4a
Codeecoffee <try.debug.mode@proton.me>
2025-05-28 15:57:08 -0700

    Manage Users Added

---
+++ ManageUsers.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace InventoryMngmtSys
+{
+    partial class ManageUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            titleLabel = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Crimson;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(titleLabel);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(844, 125);
+            panel1.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font("Franklin Gothic Medium", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            titleLabel.ForeColor = Color.White;
+            titleLabel.Location = new Point(276, 62);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(222, 39);
+            titleLabel.TabIndex = 0;
+            titleLabel.Text = "Manage Users";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Franklin Gothic Medium", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(276, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(276, 47);
+            label1.TabIndex = 1;
+            label1.Text = "Manage Users";
+            // 
+            // ManageUsers
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(844, 488);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ManageUsers";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ManageUsers";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label titleLabel;
+    }
+}
